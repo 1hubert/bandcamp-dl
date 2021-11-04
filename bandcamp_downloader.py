3008f95be158dfbe33eea8a036ed9f74ef9a6495
Hubert <hrozmarynowski776@gmail.com>
2021-11-04 19:21:31 +0100

    Delete unimportant arguments for chromedriver

--- bandcamp_downloader.py
+++ bandcamp_downloader.py
@@ -30,15 +30,10 @@ def valid_name(name):
 
 # preparing the options for the chrome driver
 options = webdriver.ChromeOptions()
-#options.add_argument("headless")
+options.add_argument("headless")
 options.add_argument("--mute-audio")
 options.add_argument("--disable-extensions")
-options.add_argument("--proxy-server='direct://'")
-options.add_argument("--proxy-bypass-list=*")
 options.add_argument("--disable-gpu")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-options.add_argument("--ignore-certificate-errors")
 options.add_argument("log-level=3")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
